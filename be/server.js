c005e877a38e6304f4b8c012d307c20974d1fa3f
Lewis Simmonds <simmondslewis5@gmail.com>
2024-01-22 17:33:11 +0000

    feat: initialised the node server working with the database

---
+++ be/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+// Connect to MongoDB
+mongoose.connect("mongodb+srv://2455344:hello12345@unicluster.0xfojui.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+// Basic Route
+app.get('/', (req, res) => res.send('Hello World!'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
