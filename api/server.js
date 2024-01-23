3bddf97ceef42eec5479b3e9b60b941f2bf7b07a
Finnbar Home <2481383@dundee.ac.uk>
2024-01-23 13:05:03 +0000

    feat: added atm schema (I believe this is the correct form, may have to be modified later)

--- api/server.js
+++ api/server.js
@@ -35,6 +35,54 @@ async function run() {
 run().catch(console.dir);
 
 
+// ATM Schema
+const atmSchema = new mongoose.Schema({
+    Identification: String,
+    SupportedLanguages: [String],
+    ATMServices: [String],
+    Accessibility: [String],
+    Access24HoursIndicator: Boolean,
+    SupportedCurrencies: [String],
+    MinimumPossibleAmount: String,
+    Note: [String],
+    OtherAccessibility: [{
+        Code: String,
+        Name: String,
+        Description: String
+    }],
+    Branch: {
+        Identification: String
+    },
+    Location: {
+        LocationCategory: [String],
+        OtherLocationCategory: [{
+            Code: String,
+            Name: String,
+            Description: String
+        }],
+        Site: {
+            Identification: String,
+            Name: String
+        },
+        PostalAddress: {
+            StreetName: String,
+            TownName: String,
+            CountrySubDivision: [String],
+            Country: String,
+            PostCode: String,
+            GeoLocation: {
+                GeographicCoordinates: {
+                    Latitude: String,
+                    Longitude: String
+                }
+            }
+        }
+    }
+});
+
+const ATM = mongoose.model('ATM', atmSchema);
+
+
 // API Endpoints
 app.get("/", (req, res) => res.send("Hello World!"));
 
