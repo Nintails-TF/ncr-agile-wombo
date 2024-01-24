2c93d62f7356cd0a7981258ba2952788d1bb0a57
Lucca-c <luccaocamargo@gmail.com>
2024-01-24 18:00:09 +0000

    fix: removed variable assignment that was caught by linter

--- be/server.js
+++ be/server.js
@@ -14,7 +14,8 @@ if (process.env.NODE_ENV !== 'test') {
   .catch(err => console.log(err));
 
   const PORT = process.env.PORT || 3000;
-  const server = app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 }
 
 module.exports = app;
