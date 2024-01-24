d9aeb5602ed5c0e9b14bd6f2d3aee2e6b19003fc
FinnbarHome <finnbar.home2@gmail.com>
2024-01-24 14:57:14 +0000

    Feat(API): Now only 2 GET methods as requested by backend team

--- api/api_server.js
+++ api/api_server.js
@@ -48,143 +48,6 @@ app.get("/api/atms/:id", async (req, res) => {
     }
 });
 
-// GET ATM services by Identification
-app.get("/api/atms/:id/services", async (req, res) => {
-    try {
-        const atm = await db
-            .collection("ATMs")
-            .findOne({ Identification: req.params.id }, { projection: { ATMServices: 1 } });
-        res.json(atm.ATMServices || []);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
-
-// GET ATMs with search functionality
-app.get("/api/atms/search", async (req, res) => {
-    try {
-        const query = {}; // Construct query based on req.query parameters
-        // Example: if (req.query.location) query['Location.TownName'] = req.query.location;
-        const atms = await db.collection("ATMs").find(query).toArray();
-        res.json(atms);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
-
-// GET Supported langs by Identification
-app.get("/api/atms/:id/supported-languages", async (req, res) => {
-    try {
-        const atm = await db
-            .collection("ATMs")
-            .findOne({ Identification: req.params.id }, { projection: { SupportedLanguages: 1 } });
-        res.json(atm.SupportedLanguages || []);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
-
-app.get("/api/atms/:id/accessibility", async (req, res) => {
-    try {
-        const atm = await db
-            .collection("ATMs")
-            .findOne({ Identification: req.params.id }, { projection: { Accessibility: 1 } });
-        res.json(atm.Accessibility || []);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
-
-app.get("/api/atms/:id/supported-currencies", async (req, res) => {
-    try {
-        const atm = await db
-            .collection("ATMs")
-            .findOne({ Identification: req.params.id }, { projection: { SupportedCurrencies: 1 } });
-        res.json(atm.SupportedCurrencies || []);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
-
-
-app.get("/api/atms/:id/access-24hr", async (req, res) => {
-    try {
-        const atm = await db
-            .collection("ATMs")
-            .findOne({ Identification: req.params.id }, { projection: { Access24HoursIndicator: 1 } });
-        res.json({ Access24HoursIndicator: atm.Access24HoursIndicator || false });
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
-
-app.get("/api/atms/:id/min-amount", async (req, res) => {
-    try {
-        const atm = await db
-            .collection("ATMs")
-            .findOne({ Identification: req.params.id }, { projection: { MinimumPossibleAmount: 1 } });
-        res.json({ MinimumPossibleAmount: atm.MinimumPossibleAmount || "0" });
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
-
-app.get("/api/atms/:id/note", async (req, res) => {
-    try {
-        const atm = await db
-            .collection("ATMs")
-            .findOne({ Identification: req.params.id }, { projection: { Note: 1 } });
-        res.json(atm.Note || []);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
-
-app.get("/api/atms/:id/other-accessibility", async (req, res) => {
-    try {
-        const atm = await db
-            .collection("ATMs")
-            .findOne({ Identification: req.params.id }, { projection: { OtherAccessibility: 1 } });
-        res.json(atm.OtherAccessibility || []);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
-
-app.get("/api/atms/:id/branch", async (req, res) => {
-    try {
-        const atm = await db
-            .collection("ATMs")
-            .findOne({ Identification: req.params.id }, { projection: { Branch: 1 } });
-        res.json(atm.Branch || {});
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
-
-app.get("/api/atms/:id/location", async (req, res) => {
-    try {
-        const atm = await db
-            .collection("ATMs")
-            .findOne({ Identification: req.params.id }, { projection: { Location: 1 } });
-        res.json(atm.Location || {});
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
-
-app.get("/api/atms/:id/geolocation", async (req, res) => {
-    try {
-        const atm = await db
-            .collection("ATMs")
-            .findOne({ Identification: req.params.id }, { projection: { "Location.PostalAddress.GeoLocation.GeographicCoordinates": 1 } });
-        res.json(atm.Location?.PostalAddress?.GeoLocation?.GeographicCoordinates || {});
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
-
-
 
 // UPDATE FUNCTIONS
 // UPDATE an ATM by Identification
@@ -203,6 +66,7 @@ app.put("/api/atms/:id", async (req, res) => {
     }
 });
 
+
 // DELETE FUNCTIONS
 // DELETE an ATM by Identification
 app.delete("/api/atms/:id", async (req, res) => {
@@ -218,6 +82,7 @@ app.delete("/api/atms/:id", async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 });
+
 
 // POST FUNCTIONS
 // POST a new ATM
