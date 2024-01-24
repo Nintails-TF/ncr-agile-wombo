077a14e02ebe5036f2ab1d14b1c46a5527438b3a
Lucca-c <luccaocamargo@gmail.com>
2024-01-24 16:52:51 +0000

    fix: prevents Mongo connections during test

--- be/server.js
+++ be/server.js
@@ -2,22 +2,19 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
-// Connect to MongoDB
-mongoose.connect(
-  "mongodb+srv://2455344:hello12345@unicluster.0xfojui.mongodb.net/?retryWrites=true&w=majority"
-)
-.then(() => console.log('MongoDB Connected'))
-.catch(err => console.log(err));
-
-
 // Basic Route
 app.get('/', (req, res) => res.send('Hello World!'));
 
-// Server only starts if this module is run directly (e.g., not when running tests)
-if (require.main === module) {
+if (process.env.NODE_ENV !== 'test') {
+  // Connect to MongoDB without deprecated options
+  mongoose.connect(
+    "mongodb+srv://2455344:hello12345@unicluster.0xfojui.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
   const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  const server = app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }
 
-// Export the Express app so it can be imported in the test file
 module.exports = app;
