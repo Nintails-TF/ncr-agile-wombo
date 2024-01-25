42317857bc550a548ff26cdb6cc517cda4db3a7b
FinnbarHome <finnbar.home2@gmail.com>
2024-01-25 16:29:16 +0000

    feat(API): Moved database functions to a separate file

--- api/api_server.js
+++ api/api_server.js
@@ -1,37 +1,22 @@
-require('dotenv').config(); 
-
+// api_server.js
+require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose');
-const atmRoutes = require('./atmRoutes'); // Import ATM routes
-const branchRoutes = require('./branchRoutes'); // Import Branch routes
+const connectDB = require('./database'); // Import the database connection function
+const atmRoutes = require('./atmRoutes');
+const branchRoutes = require('./branchRoutes');
 const app = express();
 
-// MongoDB URI
-const uri = process.env.MONGO_URI;
-
-// Middleware
 app.use(express.json());
 
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+connectDB(); // Connect to the database
 
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "Connection error:"));
-db.once("open", () => {
-  console.log("Connected to the database");
-});
-
-// Use the imported routes
 app.use(atmRoutes);
 app.use(branchRoutes);
 
-// Root endpoint
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+    res.send("Hello World!");
 });
 
-// Use the PORT environment variable
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`API server listening on port ${PORT}!`));
+
---
+++ api/database.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        const uri = process.env.MONGO_URI;
+        await mongoose.connect(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("Connected to the database");
+    } catch (error) {
+        console.error("Connection error:", error.message);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
