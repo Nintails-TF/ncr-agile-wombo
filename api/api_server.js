feddace3a3a32a73342d43358c684c8cd5302bb0
FinnbarHome <finnbar.home2@gmail.com>
2024-01-25 16:39:07 +0000

    feat(API): Improved some error handling in api_server.js

--- api/api_server.js
+++ api/api_server.js
@@ -1,4 +1,3 @@
-// api_server.js
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./database'); // Import the database connection function
@@ -13,8 +12,20 @@ connectDB(); // Connect to the database
 app.use(atmRoutes);
 app.use(branchRoutes);
 
+// Root Endpoint
 app.get("/", (req, res) => {
     res.send("Hello World!");
+});
+
+// 404 Error Handler (for any unhandled routes)
+app.use((req, res, next) => {
+    res.status(404).send("Resource not found");
+});
+
+// Generic Error Handler
+app.use((error, req, res, next) => {
+    console.error(error.stack);
+    res.status(500).send('Something broke!');
 });
 
 const PORT = process.env.PORT || 3000;
