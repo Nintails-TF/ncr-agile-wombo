c08adf1c1e769edf544e04b54e1dac87414bfc41
FinnbarHome <finnbar.home2@gmail.com>
2024-01-30 17:34:13 +0000

    fix(BE): Fixed dotenv problems

--- be/server.js
+++ be/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./database'); // Assuming you have this module for DB connection
