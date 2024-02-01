50aaf8ede68cf629e9993c2651b81ef5dfca13d1
Ben Sloan <mrbensloan@gmail.com>
2024-02-01 16:53:33 +0000

    feat(be): Hosting the backend and frontend on GAE

--- be/server.js
+++ be/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const path = require("path");
 const cors = require("cors");
 const routes = require("./routes");
 const bodyParser = require("body-parser");
@@ -8,6 +9,7 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "..", "fe")));
 
 // Use the routes defined in routes.js
 app.use("/", routes);
