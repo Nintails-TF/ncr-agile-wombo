02ca397bd8b243d9f297d89a134e18cab84be05f
FinnbarHome <finnbar.home2@gmail.com>
2024-02-02 12:57:20 +0000

    fix(API): changed per one minute, so not to cause issues but still has functional rate limiting

--- api/api_server.js
+++ api/api_server.js
@@ -11,7 +11,7 @@ const rateLimit = require('express-rate-limit');
 
 // Rate Limiting Middleware
 const apiLimiter = rateLimit({
-    windowMs: 10 * 60 * 1000, // 1 minutes
+    windowMs: 1 * 60 * 1000, // 1 minutes
     max: 1000, // limit each IP to 1000 requests per windowMs
     message: 'Too many requests from this IP, please try again after a delay.'
 });
